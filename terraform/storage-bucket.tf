96c3930a0521ec1caa21ea95325b5d6574ebc9f0
Ilya Manakhov <trimon90@gmail.com>
2019-12-15 18:35:59 +0300

    fix

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -7,7 +7,7 @@ module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.3.0"
   # Имена поменяйте на другие
-  name = ["storage-bucket-otus-trimon90", "storage-bucket-otus-trimon99"]
+  name = "storage-bucket-otus-trimon90"
 }
 output storage-bucket_url {
   value = module.storage-bucket.url
